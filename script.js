11edec66910e3d7ebdb5b2d074087837886689df
S3ncillita <rodrigonoah1@gmail.com>
2024-01-07 22:33:28 -0600

    modificado para celulares

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-var swiper = new Swiper(".mySwiper",{
-effect:"coverflow",
-grabCursor:true,
-centeredSlides:true,
-slidesPerView:"auto",
-coverflowEffect:{
-    rotate:15,
-    strech:0,
-    depth:300,
-    modifier:1,
-    slideShadows:true,
-
-},
-loop:true,
-
-
-});
+alert("BIENVENIDO A LA PAGINA ")
